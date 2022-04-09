8c4f26d0f4f4c6e5847d3b8ab3faadfa14c25023
Yadukrishnan2002 <72332872+Yadukrishnan2002@users.noreply.github.com>
2022-04-09 21:54:37 +0530

    coded the basic landing page

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Index</title>
+    <link rel = "stylesheet" type= "text/css" href = "style1(index).css">
+</head>
+<body>
+
+    <header>
+        <img class =  "logo" src="Essentials/icons8-google-48.png" alt="">
+        <nav>
+            <ul class = "nav_links">
+                <li><a href = "#">For Employers</a></li>
+                <li><a href = "#">For Employers</a></li>
+            </ul>
+        </nav>
+    
+
+    </header>
+
+    <div class="login-box">
+        
+        <form action="" method = "POST">
+            <div class="login-box-input">
+                <label for="email"><b>Email</b></label>
+                <br>
+                <input type="text" name = "email">
+                <br>
+                <label for="password"><b>Password</b></label>
+                <br>
+                <input type="password" name = "password">
+                <br>
+                
+            </div>
+            <input type="submit" value = "Login">
+            <a class = "join-button" href = ""><Button >Join Webname</Button></a>
+        </form>
+            
+        
+
+    </div>
+
+
+    
+</body>
+</html>
