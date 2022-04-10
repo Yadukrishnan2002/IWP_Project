a52a7a2e9977243dbad8ae27f477f27162879513
Yadukrishnan2002 <72332872+Yadukrishnan2002@users.noreply.github.com>
2022-04-10 11:57:43 +0530

    Company Sign Up page
    Need to make some more changes 
    Move to candidate signup page when clicking on For Employees button on the top

---
+++ Company_SignUp_Page.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Index</title>
+    <link rel = "stylesheet" type= "text/css" href = "style3 ( Company_SignUp_Page).css">
+</head>
+<body>
+
+    <header>
+        <img class =  "logo" src="Essentials/icons8-google-48.png" alt="">
+        <nav>
+            <ul class = "nav_links">
+                <li><a href = "Company_SignUp_Page.php">For Employers</a></li>
+                <li><a href = "#">For Employees</a></li>
+            </ul>
+        </nav>
+    
+
+    </header>
+
+    <div class="login-box">
+        
+        <form action="" method = "POST">
+            <div class="login-box-input">
+
+                <div class="name-input-label">
+                    <span class = "name-input-label-firstname">
+                        <label for="firstname">First Name</label>
+                        <br>
+                    </span>
+                    
+                    <span class = "name-input-label-firstname">
+                        <label for="lastname">Last Name</label>
+                        <br>
+                    </span>
+                    
+                </div>
+
+                <div class="name-input-field">
+                    <span class = "name-input-field-firstname">
+                        <input type="text" name = "firstname">
+                        <br>
+
+                    </span>
+                    
+
+
+                    <span class = "name-input-field-firstname">
+                        <input  type="text" name = "lastname">
+                        <br>
+
+                    </span>
+                    
+
+                </div>
+
+                <div class="single-row1">
+                    <span class = "single-column1">
+                        <label for="Company Name">Company Name</label>
+                        <br>
+
+                    </span>
+                    <span class = "single-column1">
+                        <label for="Head Office Location">Head office location</label>
+                        <br>
+                    </span>
+                </div>
+
+                <div class="single-row2">
+                    <span class = "single-column2">
+                        <input type="text" name = "Company Name">
+                        <br>
+                    </span>
+                    <span class = "single-column2">
+                        <input  type="text" name = "Head Office Location">
+                        <br>
+
+                    </span>
+
+                </div>
+                
+                
+                   
+
+                
+                <div class="email-input">
+                    <label for="email"><b>Email</b></label>
+                    <br>
+                    <input type="text" name = "email">
+                    <br>
+
+                </div>
+
+                <div class="password-input">
+                    <div class = "password-input-label">
+                        <span class = "password-input-label-first">
+                            <label for="password"><b>Password</b></label>
+                            <br>
+                        </span>
+
+                        <span class = " password-input-label-first">
+                            <label for="confirm-password">Confirm Password</label>
+                            <br>
+                        </span>
+                        
+                        
+
+                    </div>
+
+                    <div class = "password-input-field">
+                        <span class = "password-input-field-first">
+                            <input type="password" name = "password">
+                            <br>
+
+                        </span>
+                        
+                        <span class = "password-input-field-first">
+                            <input type="password" name = "confirm-password">
+                            <br>
+
+                        </span>
+                        
+                    </div>
+                   
+                    
+
+                </div>
+                
+                
+                
+            </div>
+
+            <input class = "submit-button" type="submit" value = "Sign Up">
+            
+        </form>
+            
+        
+
+    </div>
+
+
+    
+</body>
+</html>
