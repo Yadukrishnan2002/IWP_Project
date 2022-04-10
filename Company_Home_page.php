38eba1d8ef2da596236a2562087c27a413719c8a
Yadukrishnan2002 <72332872+Yadukrishnan2002@users.noreply.github.com>
2022-04-10 23:35:26 +0530

    Company Home page basic code

---
+++ Company_Home_page.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website name</title>
+
+    <link rel = "stylesheet" type= "text/css" href = "style4 (Company_Home_page).css">
+
+</head>
+<body>
+
+    <header>
+        <img class =  "logo" src="Essentials/icons8-google-48.png" alt="">
+        <nav>
+            <ul class = "nav_links">
+                <li><a href = "Company_SignUp_Page.php">For Employers</a></li>
+                <li><a href = "#">For Employees</a></li>
+            </ul>
+        </nav>
+    
+
+    </header>
+
+    <div class="profile-box">
+        <div class="profile-img">
+            <img src="Essentials/icons8-google-48.png" width = "200 " height = " 200"  >
+        </div>
+        <div class="profile-name">
+            Google
+        </div>
+        <div class="location-followers">
+            Mountain View, CA &emsp;&emsp; 30.5M followers
+        </div>
+        
+        
+        
+    </div>
+    
+    <div class="analytics-box">
+        <div class="analytics-title">
+            <u>Analytics</u>
+        </div>
+        <div class="analytics-details">
+            <span class = "analytics-details-text">
+                No of Job openings created: 40
+                <br><br>
+            </span>
+            <span class = "analytics-details-text">
+                No of candidates applied: 246k
+                <br><br>
+            </span>
+            <span class = "analytics-details-text">
+                No of candidates got selected: 542
+                <br><br>
+            </span>
+            <span class = "analytics-details-text">
+                Selection Ratio: 11 : 5000
+                <br><br>
+            </span>
+            
+           
+            
+            
+            
+        </div>
+    </div>
+
+    <center>
+    <div class="jobPost-container">
+        
+        <span class = "Your-job-post-label">
+            Your Job posts
+        </span>
+        <span class = "add-jobPost-button">
+          
+            <button><p class = "jobPost-btn">Create Job Post</p></button>
+        </span>
+
+        <div class="job-card">
+            <div class="single-row1">
+                <span class = "single-column1">
+                    <span class = "job-title-label">
+                        Job title: 
+                    </span>
+                    <span class = "job-title">
+                        Software Developer 
+
+                    </span>
+
+                </span>
+                
+
+                <span class = "single-column1">
+                    <span class = "Date-label">
+                        Post created on:
+
+                    </span>
+                    <span class = "Date">
+                        15-4-22
+                        <br>
+                        <br>
+                        <br>
+
+                    </span>
+
+                </span>
+                
+
+            </div>
+            
+            <div class="single-row1">
+                <span class = "single-column1">
+                    <span class = "location-label">
+                        Location: 
+
+                    </span>
+                    <span class = "location">
+                        Mountain View, CA
+
+                    </span>
+                </span>
+
+                <span class = "single-column1">
+                    <span class = "pay-scale-label">
+                        Pay scale:
+                    </span>
+                    <span class = "pay-scale">
+                        100k-140k $
+                        <br>
+                        <br>
+                        
+                    </span>
+                </span>
+            </div>
+
+            
+
+            <div class="single-row1">
+                <span class = "single-column1">
+                    <span class = "no-of-applicants-label">
+                        No of applicants:
+
+                    </span>
+                    <span class = "no-of-applicants">
+                        8.1 K
+                    </span>
+                </span>
+
+                <span class = "single-column1">
+                    <span class = "btn">
+                        <button><p class = "btn-label">View details</p></button>
+                    </span>
+                </span>
+            </div>
+            
+            
+        </div>
+
+        <div class="job-card">
+            <div class="single-row1">
+                <span class = "single-column1">
+                    <span class = "job-title-label">
+                        Job title: 
+                    </span>
+                    <span class = "job-title">
+                        Software Developer 
+
+                    </span>
+
+                </span>
+                
+
+                <span class = "single-column1">
+                    <span class = "Date-label">
+                        Post created on:
+
+                    </span>
+                    <span class = "Date">
+                        15-4-22
+                        <br>
+                        <br>
+                        <br>
+
+                    </span>
+
+                </span>
+                
+
+            </div>
+            
+            <div class="single-row1">
+                <span class = "single-column1">
+                    <span class = "location-label">
+                        Location: 
+
+                    </span>
+                    <span class = "location">
+                        Mountain View, CA
+
+                    </span>
+                </span>
+
+                <span class = "single-column1">
+                    <span class = "pay-scale-label">
+                        Pay scale:
+                    </span>
+                    <span class = "pay-scale">
+                        100k-140k $
+                        <br>
+                        <br>
+                        
+                    </span>
+                </span>
+            </div>
+
+            
+
+            <div class="single-row1">
+                <span class = "single-column1">
+                    <span class = "no-of-applicants-label">
+                        No of applicants:
+
+                    </span>
+                    <span class = "no-of-applicants">
+                        8.1 K
+                    </span>
+                </span>
+
+                <span class = "single-column1">
+                    <span class = "btn">
+                        <button><p class = "btn-label">View details</p></button>
+                    </span>
+                </span>
+            </div>
+            
+            
+        </div>
+
+        <div class="job-card">
+            <div class="single-row1">
+                <span class = "single-column1">
+                    <span class = "job-title-label">
+                        Job title: 
+                    </span>
+                    <span class = "job-title">
+                        Software Developer 
+
+                    </span>
+
+                </span>
+                
+
+                <span class = "single-column1">
+                    <span class = "Date-label">
+                        Post created on:
+
+                    </span>
+                    <span class = "Date">
+                        15-4-22
+                        <br>
+                        <br>
+                        <br>
+
+                    </span>
+
+                </span>
+                
+
+            </div>
+            
+            <div class="single-row1">
+                <span class = "single-column1">
+                    <span class = "location-label">
+                        Location: 
+
+                    </span>
+                    <span class = "location">
+                        Mountain View, CA
+
+                    </span>
+                </span>
+
+                <span class = "single-column1">
+                    <span class = "pay-scale-label">
+                        Pay scale:
+                    </span>
+                    <span class = "pay-scale">
+                        100k-140k $
+                        <br>
+                        <br>
+                        
+                    </span>
+                </span>
+            </div>
+
+            
+
+            <div class="single-row1">
+                <span class = "single-column1">
+                    <span class = "no-of-applicants-label">
+                        No of applicants:
+
+                    </span>
+                    <span class = "no-of-applicants">
+                        8.1 K
+                    </span>
+                </span>
+
+                <span class = "single-column1">
+                    <span class = "btn">
+                        <button><p class = "btn-label">View details</p></button>
+                    </span>
+                </span>
+            </div>
+            
+            
+        </div>
+
+    </div>
+</center>
+
+
+    
+</body>
+</html>
